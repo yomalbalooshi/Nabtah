62460f852e157bdfbfd92e400049186bc93fa231
AliElamir <ali.elamiir.a@gmail.com>
2024-03-21 00:05:43 +0300

    update homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar'
 const App = () => {
   return (
     <div>
-      <h1 className=" text-cyan-900 text-3xl">Nabtah</h1>
+      <h1 className=" text-cyan-900 text-3xl text-center">Nabtah</h1>
       <NavBar />
     </div>
   )
