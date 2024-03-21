bc57dcd39b830462c1120adb062271ab423c81ce
AliElamir <ali.elamiir.a@gmail.com>
2024-03-21 07:02:57 +0300

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Client from './services/api'
 import Home from './pages/Home'
 import NavBar from './components/NavBar'
 import ProduceForm from './pages/ProduceForm'
 import ServiceForm from './pages/ServiceForm'
 import ToolForm from './pages/ToolForm'
 import PackageForm from './pages/PackageForm'
+import Landing from './pages/Landing'
 
 const App = () => {
+  const [vendors, setVendors] = useState([''])
+
+  useEffect(() => {
+    const getVendors = async () => {
+      const response = await Client.get('/vendor')
+      console.log(response.data)
+      setVendors(response.data)
+    }
+    getVendors()
+  }, [])
+
   return (
     <div>
       <h1 className=" text-cyan-900 text-3xl text-center">Nabtah</h1>
       <NavBar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home vendors={vendors} />} />
           <Route path="/produceform" element={<ProduceForm />} />
           <Route path="/toolform" element={<ToolForm />} />
           <Route path="/serviceform" element={<ServiceForm />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-const Home = () =>{
+import { useState, useEffect } from 'react'
+
+const Home = ({ vendors }) => {
   return (
     <div>
-      <h3>Home</h3>
+      <div>
+        <h1>Your garden's second home</h1>
+        <h4>One stop for all your gardening needs</h4>
+      </div>
+      <div>
+        <h2>Hottest shops right now!</h2>
+        <div>
+          {vendors.map((vendor) => (
+            <div key={vendor._id}>
+              <h2>{vendor.name}</h2>
+              <p>{vendor.location}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
